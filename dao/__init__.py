ce38e81893ffe093af88bdb3c7d5811e7b047a0a
renegadelha <renegadelha@gmail.com>
2024-03-02 16:02:56 -0300

    v3

--- dao/__init__.py
+++ dao/__init__.py
@@ -1,11 +1,26 @@
 
 
-def checarlogin(usuario, senha):
-    if usuario == 'rene@rene.com' and senha == '123':
-        return True
-    else:
-        return False
+def checarlogin(users:list, usuario, senha):
+    for user in users:
+        if user['email'] == usuario and user['senha'] == senha:
+            return True
+    return False
 
-def inserir_contato(nome, email, comentario):
+def cadastrarusuario(users:list, nome, idade, email, senha):
+    novousuario = {'nome':nome, 'idade':idade, 'email':email, 'senha': senha}
+    if not usuarioexiste(users, email):
+        users.append(novousuario)
+        return True #deu certo inserir o novo usuario no banco
+    else:
+        return False #deu ruim pq ja tinha um user com este email
+
+def usuarioexiste(users:list, email):
+    for user in users:
+        if user['email'] == email:
+            return True  # já existe um usuario com este login
+    return False # nao existe ninguem com este login
+
+
+def registrar_contato(nome, email, comentario):
     print('ok')
     return True
--- main.py
+++ main.py
@@ -1,39 +1,67 @@
 from flask import *
 import dao
 
+usuarios = []
+
 #isntancia o servidor flask
 app = Flask(__name__)
-app.secret_key = 'lkjlkJSkldja5324kdsdhsj-dsa12'
+app.secret_key = '3j45h3j2k4h5kj3h45h23JHGJHgh'
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/inscricao')
+def inscricao():
+    return render_template('inscricao.html')
+
+@app.route('/cadastrarusuario', methods=['POST'])
+def cadastrar_usuario():
+
+    nome = request.form.get('nomeusuario')
+    idade = request.form.get('idadeusuario')
+    email = request.form.get('emailusuario')
+    senha = request.form.get('senhausuario')
+
+    if dao.cadastrarusuario(usuarios, nome, idade, email, senha):
+        return render_template('home.html', msg='Usuário inserido com sucesso')
+    else:
+        return render_template('home.html', msg='Usuário já existe')
+
+
 @app.route('/verificarlogin', methods=['POST'])
 def verificar_login():
     user = request.form.get('emailusuario')
     senha = request.form.get('senhausuario')
-    nome_user = user.split('@')[0]
 
-    if dao.checarlogin(user, senha):
-        session['email'] = user
-        session['nome'] = nome_user
-        return render_template('logado.html', email=nome_user)
+    if dao.checarlogin(usuarios, user, senha):
+        session['idusuario'] = user
+        return render_template('logado.html', email=user)
     else:
         return render_template('errologin.html')
 
 
 @app.route('/entraremcontato')
 def mostrarpaginacontato():
-    return render_template('contato.html', nome=session['nome'], email=session['email'])
+    if session.get('idusuario') != None:
+        return render_template('contato.html', email=session['idusuario'])
+    else:
+        return render_template('home.html')
 
-@app.route('/inserircontato', methods=['POST'])
+@app.route('/inserircontato', methods=['POST','GET'])
 def inserircontato():
-    nome = request.form.get('nome')
-    email = request.form.get('email')
-    texto = request.form.get('texto')
 
-    if dao.inserir_contato(nome, email, texto):
+
+    if request.method == 'POST':
+        nome = request.form.get('nome')  # POST
+        email = request.form.get('email')
+        texto = request.form.get('texto')
+    else:
+        nome = request.args.get('nome')  # POST
+        email = request.args.get('email')
+        texto = request.args.get('texto')
+
+    if dao.registrar_contato(nome, email, texto):
         return render_template('logado.html')
     else:
         #criar pagina de erro de contato
